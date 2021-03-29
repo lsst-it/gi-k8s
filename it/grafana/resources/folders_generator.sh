831bcaf12cacad2fca11f479626b45cc254dbea9
Heinrich Reinking <hreinkinge@gmail.com>
2021-03-29 12:50:09 -0300

    force ID at folder creation

--- it/grafana/resources/folders_generator.sh
+++ it/grafana/resources/folders_generator.sh
@@ -6,11 +6,8 @@ URL="$(kubectl -n it-grafana get ingress grafana -ojson | jq -r '.spec.tls[0].ho
 USER="$(kubectl -n ${NAMESPACE} get secret grafana-credentials -ojson | jq -r '.data["admin-user"]' | base64 --decode)"
 PASSWORD="$(kubectl -n ${NAMESPACE} get secret grafana-credentials -ojson | jq -r '.data["admin-password"]' | base64 --decode)"
 POD="$(kubectl get -n ${NAMESPACE} pods -o json | jq -r '.items[].metadata.name')"
-Folders=(servers clusters services)
-counter=3
 
 /usr/bin/kubectl wait --for=condition=ready pod -n ${NAMESPACE} ${POD} > /dev/null 2>&1
-for folder in "${Folders[@]}"; do
-    /usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":\"$counter\",\"title\":\"$folder\"}" > /dev/null 2>&1
-    counter=$((counter+1))
-done
+/usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":null,\"uid\":\"fr5kaxQGz\",\"title\":\"clusters\"}" > /dev/null 2>&1
+/usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":null,\"uid\":\"xZtkaxwMk\",\"title\":\"services\"}" > /dev/null 2>&1
+/usr/bin/curl -k -u ${USER}:${PASSWORD} -H "Content-Type: application/json" -X POST https://${URL}/api/folders -d "{\"id\":null,\"uid\":\"NbYQ1AwMz\",\"title\":\"servers\"}" > /dev/null 2>&1
